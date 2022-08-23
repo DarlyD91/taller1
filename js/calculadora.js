34acb309e38c732c5b28a9927d6b11cd72a17947
DarlyD91 <dydiaz46@misena.edu.co>
2022-08-23 12:40:12 -0500

    Puntos taller

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,26 +1,26 @@
 function calcular(){
-    alert(`Entre a la función`)
     let num1=parseInt(document.getElementById("num1").value);
     let num2=parseInt(document.getElementById("num2").value);
     let op=document.getElementById("operacion").value;
+    let res;
 
     console.log(`El num 1 es ${num1} y el numero 2 ${num2} la suma es ${op}`);
 
     switch(op){
         case "1":
-            let res=num1+num2;
-            alert(`El num 1 es ${num1} y el numero 2 ${num2} la suma es ${res}`);
+            res=num1+num2;
+            alert(`El resultado de la suma es ${res}`);
         break;
         case "2":
-            res=num1-num2
+            res=num1-num2;
             alert(`El resultado de la resta es ${res}`);
         break;
         case "3":
-            res=num1*num2
+            res=num1*num2;
             alert(`El resultado de la multiplicacion es ${res}`);
         break;
         case "4":
-            res=num1/num2
+            res=num1/num2;
             alert(`El resultado de la division es ${res.toFixed(2)}`);
         break;
         default:
---
+++ js/temperatura.js
@@ -0,0 +1,10 @@
+function temperatura(){
+    let temp=parseInt(document.getElementById("temp1").value);
+    let num2=parseInt(document.getElementById("num2").value);
+    let op=document.getElementById("operacion").value;
+    let res;
+
+    console.log(`El num 1 es ${num1} y el numero 2 ${num2} la suma es ${op}`);
+
+    
+}
